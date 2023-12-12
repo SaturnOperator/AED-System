81f6873f383332aa5a319751d1c40fa2062931ba
Yijing Pang <76888755+WwW88688@users.noreply.github.com>
2023-12-11 22:07:35 -0500

    Stage6: reset shock# and time

--- src/Stage6.cpp
+++ src/Stage6.cpp
@@ -11,12 +11,19 @@ bool Stage6::start(){ // @ Override from StageManger
     screen->setStage(stage); // Set Screen to this stage
 
     // Code here
-
+    if(!isError()){
+        // Update the status to indicate the stage is active
+        setStatus(Stage6PostUse::INIT);
+        return false;
+    }
     return true;
 }
 
 void Stage6::stop(){
-    // Code here
+    // Code here    
+    setStatus(Stage6PostUse::DONE);
+    qInfo() <<"stage6: done";
+
 }
 
 bool Stage6::setStatus(Stage6PostUse s){ // @ Overload from StageManger
@@ -25,13 +32,19 @@ bool Stage6::setStatus(Stage6PostUse s){ // @ Overload from StageManger
 }
 
 bool Stage6::nextStage(){ // @ Override from StageManger
-    controller->setStage(stage); // Set AED controller's stage to this one
+    controller->setStage(Stage::POWER); // Set AED controller's stage to this one
 
     // Code here
+    screen->setStage(Stage::POWER);
 
     return true;
 }
 
 void Stage6::step(){
-    
+    if(!isActive()){
+        return;
+    }
+
+    // reset shock num and AED setting(time)
+    controller->setShockCount(0);
 }
